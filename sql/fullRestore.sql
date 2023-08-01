753a40070e23f662a274dfbffff5cab2b67221a3
Bernie Yanos <21007971+bernieYanos@users.noreply.github.com>
2023-08-01 08:01:40 -0400

    added percentage to varietal table

--- sql/fullRestore.sql
+++ sql/fullRestore.sql
@@ -45,10 +45,11 @@ CREATE TABLE Product (
 CREATE TABLE Varietal_Tags (
   Product_ID INT(9) NOT NULL,
   Varietal_ID INT(9) NOT NULL,
+  Varietal_Percentage INT(3),
   CONSTRAINT Varietal_Tags_PK PRIMARY KEY (Product_ID, Varietal_ID),
   CONSTRAINT Varietal_Tags_FK1 FOREIGN KEY (Product_ID) REFERENCES Product(Product_ID),
   CONSTRAINT Varietal_Tags_FK2 FOREIGN KEY (Varietal_ID) REFERENCES Varietal(Varietal_ID)
-) Auto_Increment = 1;
+);
 
 INSERT INTO
   Category (
@@ -118,9 +119,13 @@ VALUES
   );
 
 INSERT INTO
-  Varietal_Tags (`Product_ID`, `Varietal_ID`)
+  Varietal_Tags (
+    `Product_ID`,
+    `Varietal_ID`,
+    `Varietal_Percentage`
+  )
 VALUES
-  (1, 1),
-  (1, 2),
-  (1, 3),
-  (2, 4);
+  (1, 1, null),
+  (1, 2, null),
+  (1, 3, null),
+  (2, 4, 100);
