8b0505bda2b69e0acfe603ac82eed7a9c438a129
Bernie Yanos <21007971+bernieYanos@users.noreply.github.com>
2023-08-02 10:05:15 -0400

    updated varietal and vintage section

--- includes/wine-insert.inc.php
+++ includes/wine-insert.inc.php
@@ -1,7 +1,13 @@
 <?php
 if ($exists > 0) {
-  $sql = "SELECT Product_ID, Product_Name, Product_Quantity, Product_Image_Path, Product_Description, Vintage FROM Product WHERE Sub_Category_ID = $subCategory";
+  $sql = "SELECT P.Product_ID, P.Product_Name, P.Product_Quantity, P.Product_Image_Path, P.Product_Description, P.Vintage, VT.Varietal_ID, VT.Varietal_Percentage, V.Varietal_Name
+  FROM Product P
+  LEFT JOIN Varietal_Tags VT ON P.Product_ID = VT.Product_ID
+  LEFT JOIN Varietal V ON vt.Varietal_ID = V.Varietal_ID
+  WHERE P.Sub_Category_ID = $subCategory";
   $result = mysqli_query($con, $sql);
+
+  $productData = array();
 
   echo '<div class="accordion" id="wineAccordion">';
   echo '<div class="row py-2 border-bottom spirit-container">';
@@ -10,6 +16,34 @@ if ($exists > 0) {
   echo '</div>';
 
   while ($row = mysqli_fetch_assoc($result)) {
+    $productID = $row['Product_ID'];
+
+    if (!isset($productData[$productID])) {
+      $productData[$productID] = array(
+        'Product_ID' => $row['Product_ID'],
+        'Product_Name' => $row['Product_Name'],
+        'Product_Quantity' => $row['Product_Quantity'],
+        'Product_Image_Path' => $row['Product_Image_Path'],
+        'Product_Description' => $row['Product_Description'],
+        'Vintage' => $row['Vintage'],
+        'Varietals' => array()
+      );
+    }
+
+    if ($row['Varietal_ID']) {
+      $varietalData = array(
+        'Varietal_ID' => $row['Varietal_ID'],
+        'Varietal_Percentage' => $row['Varietal_Percentage'],
+        'Varietal_Name' => $row['Varietal_Name']
+      );
+
+      $productData[$productID]['Varietals'][] = $varietalData;
+    }
+  }
+
+  // print_r($productData);
+
+  foreach ($productData as $row) {
     if ((int)$row['Product_Quantity'] != 0) {
       echo '<div class="row py-2 border-bottom">';
       echo '<div class="col-1 p-0 text-white d-flex align-items-center justify-content-center">';
@@ -25,19 +59,37 @@ if ($exists > 0) {
       echo '<div class="row">';
       echo '<div class="col"><img src="uploads/' . $row["Product_Image_Path"] . '" class="img-fluid"></div>';
       echo '<div class="col-8">';
-      echo '<p class="mb-2">' . nl2br($row["Product_Description"]) . '</p>';
-      echo '<table class="table">';
-      echo '<tbody>';
-      echo '<tr>';
-      echo '<th>Vintage</th>';
-      echo '<td>' . $row["Vintage"] . '</td>';
-      echo '</tr>';
-      echo '<tr>';
-      echo '<th>Varietal Composition</th>';
-      echo '<td>' . $row["Vintage"] . '</td>';
-      echo '</tr>';
-      echo '</tbody>';
-      echo '</table>';
+      echo '<p class="mb-4">' . nl2br($row["Product_Description"]) . '</p>';
+      echo '<div class="row border-top border-bottom m-0">';
+      echo '<div class="col-6 d-flex justify-content-left align-items-center">';
+      echo '<h4 class="accordion-interior-header my-2">Vintage</h4>';
+      echo '</div>';
+      echo '<div class="col-6 d-flex justify-content-end align-items-center">';
+      if ($row['Vintage'] != null) {
+        echo '<p class="my-2 accordion-table-data">' . $row["Vintage"] . '</p>';
+      } else {
+        echo '<p class="my-2 accordion-table-data">None</p>';
+      }
+      echo '</div>';
+      echo '</div>';
+      echo '<div class="row border-bottom m-0">';
+      echo '<div class="col-6 d-flex justify-content-left align-items-center">';
+      echo '<h4 class="accordion-interior-header my-2">Varietal</h4>';
+      echo '</div>';
+      echo '<div class="col-6 d-flex justify-content-end align-items-center">';
+      echo '<div class="row">';
+      foreach ($row['Varietals'] as $varietal) {
+        echo '<div class="col-12">';
+        if ($varietal['Varietal_Percentage'] != null) {
+          echo '<p class="my-2 accordion-table-data d-flex justify-content-end">' . $varietal['Varietal_Percentage'] . '% ' . $varietal['Varietal_Name'] . '</p>';
+        } else {
+          echo '<p class="my-2 accordion-table-data  d-flex justify-content-end">' . $varietal['Varietal_Name'] . '</p>';
+        }
+        echo '</div>';
+      }
+      echo '</div>';
+      echo '</div>';
+      echo '</div>';
       echo '<div class="row mt-5">';
       echo '<div class="col-4 d-flex justify-content-center">';
       echo '<form class="justify-content-center update-spirit" method="POST" action="update-quantity.php">';
