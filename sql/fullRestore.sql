82c8243190e91d28dd01f1a6f3aa0560b59271eb
Bernie Yanos <21007971+bernieYanos@users.noreply.github.com>
2023-08-03 06:49:41 -0400

    updated sub category with foreign key

--- sql/fullRestore.sql
+++ sql/fullRestore.sql
@@ -7,9 +7,11 @@ CREATE TABLE Category (
 
 CREATE TABLE Sub_Category (
   Sub_Category_ID INT(9) NOT NULL Auto_Increment,
+  Category_ID INT(9) NOT NULL,
   Sub_Category_Name VARCHAR(255) NOT NULL,
   Sub_Category_Description LONGTEXT,
-  CONSTRAINT Sub_Category_PK PRIMARY KEY (Sub_Category_ID)
+  CONSTRAINT Sub_Category_PK PRIMARY KEY (Sub_Category_ID),
+  CONSTRAINT Sub_Category_FK1 FOREIGN KEY (Category_ID) REFERENCES Category(Category_ID)
 ) Auto_Increment = 1;
 
 CREATE TABLE Source (
@@ -60,13 +62,14 @@ VALUES
 
 INSERT INTO
   Sub_Category (
+    `Category_ID`,
     `Sub_Category_Name`,
     `Sub_Category_Description`
   )
 VALUES
-  ('White', null),
-  ('Red', null),
-  ('Blush', null);
+  (1, 'White', null),
+  (1, 'Red', null),
+  (1, 'Blush', null);
 
 INSERT INTO
   Source (`Source_Name`, `Source_Description`)
